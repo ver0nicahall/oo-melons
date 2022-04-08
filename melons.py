f7883f1d3971ddc6119ec8322fae384938a990b9
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-04-08 14:03:09 -0500

    Finished splurge and rush hour pricing

--- melons.py
+++ melons.py
@@ -1,5 +1,10 @@
 """Classes for melon orders."""
 
+from random import randint
+from datetime import datetime
+#access day of the week 
+
+
 class AbstractMelonOrder:
     """A melon order."""
     
@@ -13,21 +18,42 @@ class AbstractMelonOrder:
         self.tax = tax
         self.shipped = False
 
-    def get_total(self):
-        """Calculate price, including tax."""
+    def get_base_price(self):
+        """Get a base price."""
 
-        base_price = 5
+        timestamp = (datetime.now())
+        hour = timestamp.hour
+        day_of_week = (datetime.now().weekday())
 
-        #if species is a Christmas melon
+        # Extra charge during morning rush hour (8-11am, Mon-Fri)
+        extra_charge = 4
+
+        #choose random integer between 5-9 as base price
+        base_price = randint(5,9)
+
+        if day_of_week in range(0,5) and hour in range(8, 11):
+            base_price += extra_charge
+
+        #if species is a Christmas melon, multiply by 1.5
         if self.species == "Christmas":
             base_price *= 1.5
         
+        return base_price
+    
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = self.get_base_price()
+        print('base price: ', base_price)
+
         #flat fee: international orders with < 10 melons
         flat_fee = 0
         if self.order_type == 'international' and self.qty < 10:
             flat_fee = 3
 
         total = (1 + self.tax) * self.qty * base_price + flat_fee
+        print('tax: ', self.tax * (base_price * self.qty))
+        print('flat fee: ', flat_fee)
 
         return total
 
@@ -72,3 +98,6 @@ class GovernmentMelonOrder(AbstractMelonOrder):
         """Update inspection."""
     
         self.passed_inspection = passed 
+
+class TooManyMelonsError(ValueError):
+    super().__init__()
